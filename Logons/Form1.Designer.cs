17183ea749f361c4fb2276c5c9ba4fa84cb83652
Manize <manize@web.de>
2014-02-08 20:05:23 +0100

    #1

--- Logons/Form1.Designer.cs
+++ Logons/Form1.Designer.cs
@@ -28,21 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRefresh.Location = new System.Drawing.Point(476, 428);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 0;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            // 
+            // textBoxContent
+            // 
+            this.textBoxContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxContent.Location = new System.Drawing.Point(13, 13);
+            this.textBoxContent.Multiline = true;
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.Size = new System.Drawing.Size(538, 398);
+            this.textBoxContent.TabIndex = 1;
+            this.textBoxContent.Text = "Test";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(563, 463);
+            this.Controls.Add(this.textBoxContent);
+            this.Controls.Add(this.buttonRefresh);
             this.Name = "Form1";
             this.Text = "Logons";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TextBox textBoxContent;
     }
 }
 
